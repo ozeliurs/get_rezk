0fb21d12f1b72cea6015f9c660878dd212685bbc
Ozeliurs <ozeliurs@gmail.com>
2025-01-07 15:25:42 +0100

    add stubs

---
+++ pysa/stubs/integration_test/fixture_stubs/rest_framework/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""
+TODO(T132414938) Add a module-level docstring
+"""
+
+
+from rest_framework.requests import Request  # noqa: F401
