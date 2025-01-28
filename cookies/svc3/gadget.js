51c46021e83fbd5bdf8be646e288e9baad09283c
Ozeliurs <ozeliurs@gmail.com>
2025-01-28 11:48:57 +0100

    fix(cookies): replace script tag with iframe in cookie2.html and update gadget.js to delete cookies on load

--- cookies/svc3/gadget.js
+++ cookies/svc3/gadget.js
@@ -1,6 +1,8 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", () => {
   const cookies = document.cookie.split(";");
   cookies.forEach((cookie) => {
     console.log(cookie.trim());
+    document.cookie =
+      cookie.trim() + "; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
   });
 });
